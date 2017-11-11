278a1fbc3de20234245a4686efdf2a204917d8df
zatef <zeyadatef@gmail.com>
2017-11-11 16:17:33 -0500

    added bootstrap form-for and created vidoe migration

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem "paperclip", "~> 5.0.0"
+gem 'simple_form'
+gem 'font-awesome-sass', '~> 4.7.0'
 group :development, :test do
   gem 'byebug', platform: :mri
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140224203311) do
+ActiveRecord::Schema.define(version: 20171111195353) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20140224203311) do
     t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "videos", force: :cascade do |t|
+    t.string   "title"
+    t.text     "comment"
+    t.boolean  "status"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_videos_on_user_id"
   end
 
 end
